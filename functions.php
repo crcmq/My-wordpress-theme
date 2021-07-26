5198937ffaeac8312047a1f28a3638f89cf7899a
Roger Chen <mengqiu.chen@hotmail.com>
2021-07-26 13:56:27 -0600

    Link highlight css

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
 function mytheme_setup() {
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Ubuntu+Condensed&family=Ubuntu+Mono&family=Ubuntu:ital,wght@0,300;0,400;0,500;1,300;1,400&display=swap');
     wp_enqueue_style('fontawesome',  get_template_directory_uri() . '/fontawesome/css/all.css');
+	wp_enqueue_style('highlight',  get_template_directory_uri() . '/highlight/styles/night-owl.min.css');
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_script("main", get_theme_file_uri('/js/main.js'), NULL, '1.0.0', true);
     wp_enqueue_script("highlight", get_template_directory_uri() . '/highlight/highlight.min.js');
